f72a1106aa1521fa2e5e41b774c7fbe87412fe58
goog <goutham.menon3@gmail.com>
2021-04-03 02:14:54 +0530

    modified outputs

--- script.js
+++ script.js
@@ -7,39 +7,40 @@ function computerPlay() {
 function playRound(playerSel, compSel) {
     if (playerSel == compSel)
     {
-        return "Draw";
+        return "DRAW";
     }
     else if (playerSel == "ROCK")
     {
         if (compSel == "PAPER")
-        {cs++; return "Computer Wins! Paper beats Rock.";}
+        {cs++; return "I WIN! Paper beats Rock.";}
         else if (compSel == "SCISSORS")
-        {ps++; return "Player Wins! Rock beats Scissors.";}
+        {ps++; return "YOU WIN! Rock beats Scissors.";}
     }
     else if (playerSel == "PAPER")
     {
         if (compSel == "SCISSORS")
-        {cs++; return "Computer Wins! Scissors beat Paper.";}
+        {cs++; return "I WIN! Scissors beat Paper.";}
         else if (compSel == "ROCK")
-        {ps++; return "Player Wins! Paper beats Rock.";}
+        {ps++; return "YOU WIN! Paper beats Rock.";}
     }
     else if (playerSel == "SCISSORS")
     {
         if (compSel == "ROCK")
-        {cs++; return "Computer Wins! Rock beats Scissors.";}
+        {cs++; return "I WIN! Rock beats Scissors.";}
         else if (compSel == "PAPER")
-        {ps++; return "Player Wins! Scissors beat Paper.";}
+        {ps++; return "YOU WIN! Scissors beat Paper.";}
     }
 }
 
 function game() {
-    for (let i=0; i<5; i++)
+    for (let i=1; i<=5; i++)
     {
-        let playerSel = prompt("Rock, Paper, or Scissors?");
+        let playerSel = prompt("You choose. Rock, Paper, or Scissors?");
         playerSel=playerSel.toUpperCase();
-        let compSel = computerPlay();  
-        console.log("PLAYER SELECTED: ", playerSel);
-        console.log("COMPUTER SELECTED: ", compSel);
+        let compSel = computerPlay(); 
+        console.log("ROUND ", i,":"); 
+        console.log("You Selected: ", playerSel);
+        console.log("I Selected: ", compSel);
         console.log(playRound(playerSel, compSel)); 
     }
 }
@@ -47,8 +48,8 @@ function game() {
 
 let ps=0; let cs=0;
 game();
-console.log("PLAYER SCORE: ",ps);
-console.log("COMPUTER SCORE: ",cs);
+console.log("YOUR SCORE: ",ps);
+console.log("MY SCORE: ",cs);
 if (ps>cs)
 console.log("FUCK YOU.");
 else if (cs>ps)
