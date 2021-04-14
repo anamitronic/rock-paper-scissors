53200e5de0b2abe1a2520a2c8379793296c4c3f4
goog <goutham.menon3@gmail.com>
2021-04-14 23:25:35 +0530

    Started converting to button UI with DOM producing results in the page itself instead of console. Still need to add a score counter and winner selector.

--- script.js
+++ script.js
@@ -12,30 +12,36 @@ function playRound(playerSel, compSel) {
     else if (playerSel == "ROCK")
     {
         if (compSel == "PAPER")
-        {cs++; return "I WIN! Paper beats Rock.";}
+        {//cs++; 
+        return "I WIN! Paper beats Rock.";}
         else if (compSel == "SCISSORS")
-        {ps++; return "YOU WIN! Rock beats Scissors.";}
+        {//ps++; 
+        return "YOU WIN! Rock beats Scissors.";}
     }
     else if (playerSel == "PAPER")
     {
         if (compSel == "SCISSORS")
-        {cs++; return "I WIN! Scissors beat Paper.";}
+        {//cs++; 
+        return "I WIN! Scissors beat Paper.";}
         else if (compSel == "ROCK")
-        {ps++; return "YOU WIN! Paper beats Rock.";}
+        {//ps++; 
+        return "YOU WIN! Paper beats Rock.";}
     }
     else if (playerSel == "SCISSORS")
     {
         if (compSel == "ROCK")
-        {cs++; return "I WIN! Rock beats Scissors.";}
+        {//cs++; 
+        return "I WIN! Rock beats Scissors.";}
         else if (compSel == "PAPER")
-        {ps++; return "YOU WIN! Scissors beat Paper.";}
+        {//ps++;
+        return "YOU WIN! Scissors beat Paper.";}
     }
 }
 
-function game() {
+/* function game() {
     for (let i=1; i<=5; i++)
     {
-        let playerSel = prompt("You choose. Rock, Paper, or Scissors?");
+        let playerSel = prompt("You choose. Rock, Paper, or Scissors?");  
         playerSel=playerSel.toUpperCase();
         let compSel = computerPlay(); 
         console.log("ROUND ", i,":"); 
@@ -43,10 +49,10 @@ function game() {
         console.log("I Selected: ", compSel);
         console.log(playRound(playerSel, compSel)); 
     }
-}
+}   */
 
 
-let ps=0; let cs=0;
+/* let ps=0; let cs=0;
 game();
 console.log("YOUR SCORE: ",ps);
 console.log("MY SCORE: ",cs);
@@ -55,4 +61,44 @@ console.log("FUCK YOU.");
 else if (cs>ps)
 console.log("GIT GUD SUCKA!");
 else
-console.log("WE BOTH SUCK :(");
+console.log("WE BOTH SUCK :("); */
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        game(button.id);
+    });
+});
+
+function game(playerSel) {
+    playerSel = playerSel.toUpperCase();
+    let compSel = computerPlay();
+    
+    /* console.log("You Selected: ", playerSel);
+    console.log("I Selected: ", compSel); */
+    
+    let result = playRound(playerSel, compSel);
+    console.log(result);
+
+    const body = document.querySelector('body');
+    const res = document.createElement('div');
+    res.style.cssText = 'margin: auto; align-content: center; padding: 50px;';
+
+    const pl = document.createElement('para');
+    pl.textContent = `You Selected: ${playerSel}`;
+    linebreak = document.createElement('br');
+
+    const p = document.createElement('para');
+    p.textContent = `I Selected: ${compSel}`;
+    lb = document.createElement('br');
+
+    const r = document.createElement('para');
+    r.textContent = `${result}`;
+
+    body.appendChild(res);
+    res.appendChild(pl);
+    res.appendChild(linebreak);
+    res.appendChild(p);
+    res.appendChild(lb);
+    res.appendChild(r);
+}
